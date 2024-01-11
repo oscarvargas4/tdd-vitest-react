66d726e0ac5142fb5061121c09006b2d617f8ec9
Oscar Vargas <oscar.andres.vargas1@gmail.com>
2024-01-11 15:53:46 +0100

    Completing Accordion and its tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css'
+import Accordion from './components/Accordion'
 
 function App() {
 
   return (
-    <>Hello World</>
+    <div>
+      <Accordion title="Testing title">
+        <h3>This is the h3 from the content child</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, odit?</p>
+      </Accordion>
+    </div>
   )
 }
 
--- src/components/Accordion.test.tsx
+++ src/components/Accordion.test.tsx
@@ -1,15 +1,35 @@
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 import Accordion from './Accordion';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 describe('Accordion', () => {
-  it('should render component', () => {
+  beforeEach(() => {
     render(
       <Accordion title="hello">
         <h3>My content</h3>
         <p>Some content</p>
       </Accordion>
     );
+  });
+
+  it('should show the title all the time', () => {
     expect(screen.getByText('hello')).toBeDefined();
   });
+
+  it('should not show the content at the start', () => {
+    expect(screen.queryByText(/content/i)).toBeNull();
+  });
+
+  it('should show the content when title is clicked', () => {
+    const button = screen.getByText(/open/i);
+    fireEvent.click(button);
+    expect(screen.queryAllByText(/content/i)).toBeDefined();
+  })
+
+  it('should hide the content when title is clicked', () => {
+    const button = screen.getByText(/open/i);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    expect(screen.queryByText(/content/i)).toBeNull();
+  })
 });
